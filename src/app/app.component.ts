eb411e0cdd5d52411d35764f72a6e16ffdeadca4
Vladyslav Kolodka <Vladyslav_Kolodka@epam.com>
2018-11-12 17:58:43 +0200

    Title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shop';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('appTitle') title: ElementRef;
+
+  ngAfterViewInit(): void {
+    (this.title.nativeElement as HTMLElement).innerHTML = `Shop | ${Date()}`;
+  }
 }
